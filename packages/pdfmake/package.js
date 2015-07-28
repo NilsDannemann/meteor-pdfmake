a1258a09cb5647eeae9dcee732176bcc2a1b3a3f
Nils Dannemann <ndannemann@gmail.com>
2015-07-28 16:19:40 +0200

    Added smart.json

--- packages/pdfmake/package.js
+++ packages/pdfmake/package.js
@@ -1,12 +1,12 @@
 Package.describe({
 	name: "nilsdannemann:pdfmake",
-	summary: "Adds pdfmake to your project",
-	documentation: 'readme.md',
-	version: "0.1.18",
+	summary: "Adds pdfmake to your Meteor application",
 	author: "Nils Dannemann <ndannemann@gmail.com>",
+	version: "0.1.18",
+	documentation: 'readme.md',
 	git: "https://github.com/NilsDannemann/meteor-pdfmake"
 });
 
 Package.on_use(function (api, where) {
-	api.add_files(['pdfmake.min.js', 'vfs_fonts.js'], 'client');
+	api.add_files(['build/pdfmake.min.js', 'build/vfs_fonts.js'], 'client');
 });
