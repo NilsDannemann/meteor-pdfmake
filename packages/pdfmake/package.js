32ac55a86f0c6bc24ea346298e2e2be5b71c7e47
Nils Dannemann <ndannemann@gmail.com>
2015-07-28 13:49:15 +0200

    Quick Changes

--- packages/pdfmake/package.js
+++ packages/pdfmake/package.js
@@ -1,8 +1,12 @@
 Package.describe({
-	summary: "Adds pdfmake to your project"
+	name: "nilsdannemann:pdfmake",
+	summary: "Adds pdfmake to your project",
+	documentation: 'readme.md',
+	version: "0.1.18",
+	author: "Nils Dannemann <ndannemann@gmail.com>",
+	git: "https://github.com/NilsDannemann/meteor-pdfmake"
 });
 
 Package.on_use(function (api, where) {
 	api.add_files(['pdfmake.min.js', 'vfs_fonts.js'], 'client');
-
 });
