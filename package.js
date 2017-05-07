8dbe6d6fabb86f4db7877b91df6e061b6e2480b7
jeremyfourna <jeremyfourna@gmail.com>
2017-05-07 17:47:47 +0200

    Added new files from original package

--- package.js
+++ package.js
@@ -9,5 +9,5 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.1.0.2');
-	api.addFiles(['build/pdfmake.min.js', 'build/vfs_fonts.js'], 'client');
+	api.addFiles(['build/pdfmake.min.js', 'build/vfs_fonts.js', 'build/pdfmake.min.js.map'], 'client');
 });
